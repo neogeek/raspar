f5496e346184457ce9d62bcfd82871784d8563b3
neogeek <neogeek0101@gmail.com>
2019-11-12 18:06:35 -0500

    Export all methods from library.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/raspar');
+const {fetch, requestFromUrlorCache} = require('./lib/raspar');
+const checkCacheExpiry = require('./utils/check-cache-expiry');
+const generateUUID = require('./utils/generate-uuid');
+
+module.exports = {
+    checkCacheExpiry,
+    fetch,
+    generateUUID,
+    requestFromUrlorCache
+};

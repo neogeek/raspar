f3605eab28c0e8a7ea2311f28c488055f76391e8
neogeek <neogeek0101@gmail.com>
2019-11-12 21:48:10 -0500

    Added test.

---
+++ tests/read-cache.test.js
@@ -0,0 +1,31 @@
+const {utimesSync} = require('fs');
+const {join} = require('path');
+
+const readCache = require('../utils/read-cache');
+
+beforeEach(() => {
+
+    const time = new Date();
+
+    utimesSync(
+        join(
+            __dirname,
+            '/mocks/cache.txt'
+        ),
+        time,
+        time
+    );
+
+});
+
+test(
+    'Get contents of cached file',
+    () => {
+
+        expect(readCache(join(
+            __dirname,
+            '/mocks/cache.txt'
+        ))).resolves.toMatch(/cache/u);
+
+    }
+);

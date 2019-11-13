ae5d9aa872291e42da692c25f7c5b0c6c9ab5ada
neogeek <neogeek0101@gmail.com>
2019-11-12 19:34:44 -0500

    Added tests.

---
+++ tests/check-cache-expiry.test.js
@@ -0,0 +1,42 @@
+const {join} = require('path');
+
+const checkCacheExpiry = require('../utils/check-cache-expiry');
+
+test(
+    'Check cache for missing file',
+    () => {
+
+        expect(checkCacheExpiry(join(
+            __dirname,
+            '/mocks/missing-file.txt'
+        ))).rejects.toThrowError('Cache is missing.');
+
+    }
+);
+
+test(
+    'Check cache for existing file',
+    () => {
+
+        expect(checkCacheExpiry(join(
+            __dirname,
+            '/mocks/cache.txt'
+        ))).resolves.toBeTruthy();
+
+    }
+);
+
+test(
+    'Check cache for expired file',
+    () => {
+
+        expect(checkCacheExpiry(
+            join(
+                __dirname,
+                '/mocks/cache.txt'
+            ),
+            -1
+        )).rejects.toThrowError('Cache is expired.');
+
+    }
+);
